bdd0e05cd79d0ad45a3ea3454b4ce4e56c73d7ec
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-03-27 22:20:10 -0400

    Bulb sublasses refer to TL module, traffic light opens with red on

--- traffic.rb
+++ traffic.rb
@@ -1,8 +1,15 @@
+module TL
+  Go = "#00FF30"
+  Wait = "#FFFC00"
+  Stop = "#FF0000"
+end
+
 class TrafficLight  
   include Enumerable #gives a whole bunch of methods, what does it mean to "each state in a traffic light"
+  include TL
 
   def each
-    yield [true, false, false] #toggles the which lights are on
+    yield TL::Go [true, false, false] #toggles the which lights are on
     yield [true, true, false]  #A truth table
     yield [false, false, true]
     yield [false, true, false]
@@ -15,7 +22,7 @@ class Bulb < Shoes::Shape #Inheriting from the module "Shoes", inheriting from "
   attr_accessor :top
   attr_accessor :switched_on
   
-  def initialize(stack, left, top, switched_on = false)    
+  def initialize(stack, left, top, switched_on = false)    #default assignment to
     self.stack = stack
     self.left = left    
     self.top = top
@@ -40,36 +47,36 @@ end
 
 class GoBulb < Bulb
   def bulb_colour
-      "#00FF30"
+    TL::Go
   end
 end
 
 class WaitBulb < Bulb
   def bulb_colour
-    "#FFFC00"
+    TL::Wait  
   end  
 end
 
 class StopBulb < Bulb
   def bulb_colour
-    "#FF0000"
+    TL::Stop
   end  
 end
-
-
 
 Shoes.app :title => "My Amazing Traffic Light", :width => 150, :height => 250 do
   background "000", :curve => 10, :margin => 25  
   stroke black    
   
   @traffic_light = TrafficLight.new
-  @top = Bulb.new self, 50, 40, true     #instantiating three objects of type bold
+  @top = StopBulb.new self, 50, 40, false     #instantiating three objects of type bold
   @middle = Bulb.new self, 50, 100, true # numbers are for positioning
   @bottom = Bulb.new self, 50, 160, true
   
   click do #anytime shoes detects a click anywhere on the traffic light, I'll give you a chance to do something
-    @top = StopBulb.new self, 50, 40, true
-    @middle = WaitBulb.new self, 50, 100, true
-    @bottom = GoBulb.new self, 50, 160, true
+    
+    #@top = StopBulb.new self, 50, 40, false # Need to assign the each method's Boolean to switch the correct bulbs
+    # @middle = WaitBulb.new self, 50, 100, false
+    # @bottom = GoBulb.new self, 50, 160, false
+
   end
 end
